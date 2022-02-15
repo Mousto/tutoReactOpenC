a2eb436e90018a4270465658803694236b59426e
Mous Dia <bediaraye-w@yahoo.fr>
2022-02-15 11:58:42 +0100

    ajout de fichiers testReact.js, package-lock.json, testReact.html

---
+++ js/testReact.js
@@ -0,0 +1,14 @@
+
+$(function() {
+    // function Header() {
+    //     return (<div>
+    //         <h1>La maison jungle</h1>
+    //     </div>)
+    // }
+    function MonComponent() {
+    return (<h1>Hello OpenClassrooms 👋</h1>)
+    }
+    
+    ReactDOM.render(<MonComponent />, document.getElementById("root"))
+
+});
