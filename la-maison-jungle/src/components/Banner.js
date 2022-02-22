d6e653a340533433de1ff8f3828b2b3f749627e3
Mousto <bediaraye-w@yahoo.fr>
2022-02-22 13:57:10 +0100

    Ajout dossier la-maison-jungle et le fichier infos.pages

---
+++ la-maison-jungle/src/components/Banner.js
@@ -0,0 +1,8 @@
+import logo from '../assets/logo.png';
+import '../styles/Banner.css';
+
+function Banner({ children }) {
+	return <div className='lmj-banner'>{children}</div>
+}
+
+export default Banner
---
+++ la-maison-jungle/src/components/CareScale.js
@@ -0,0 +1,24 @@
+// import Sun from '../assets/sun.svg'
+// import Water from '../assets/water.svg'
+
+function CareScale({ scaleValue, careType }) {
+	const range = [1, 2, 3]
+	const scaleType =
+		careType === 'light' ? (
+			'☀️' 
+		) : (
+			'💧'
+		)
+
+	return (
+		<div>
+			{range.map((rangeElem) =>
+				scaleValue >= rangeElem ? (
+					<span key={rangeElem.toString()}>{scaleType}</span>
+				) : null
+			)}
+		</div>
+	)
+}
+
+export default CareScale
